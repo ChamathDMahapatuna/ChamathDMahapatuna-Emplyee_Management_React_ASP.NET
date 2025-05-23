243d22f2c9d72d30b0477692fa35fea63ed1b4df
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2025-05-23 22:40:57 +0530

    CREATE

--- WebApplication1/Controllers/EmployeeController.cs
+++ WebApplication1/Controllers/EmployeeController.cs
@@ -6,7 +6,7 @@ namespace WebApplication1.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        public ActionResult Index()
         {
             List<EmployeeModel> Employee = new List<EmployeeModel>();
 
@@ -14,5 +14,34 @@ namespace WebApplication1.Controllers
             Employee = empDAO.GetAllEmployees();
             return View("Index",Employee);
         }
+        public ActionResult Details(String Id)
+        {
+            EmpDAO empDAO = new EmpDAO();   
+            EmployeeModel Employee = empDAO.GetEmployeeById(Id);
+        
+            return View("Details", Employee);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View("Create"); 
+
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(EmployeeModel employee)
+        {
+            if (ModelState.IsValid)
+            {
+                EmpDAO empDAO = new EmpDAO();
+                empDAO.AddEmployee(employee);
+               
+                return RedirectToAction("Details", new { id = employee.EEID });
+            }
+
+            return View(employee); 
+        }
+
+
     }
 }
