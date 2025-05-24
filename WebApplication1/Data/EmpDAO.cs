41ea46fda0d2c388d9ad2db810b27247f2ab9d3b
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2025-05-24 14:45:16 +0530

    read_ all_DAO

--- WebApplication1/Data/EmpDAO.cs
+++ WebApplication1/Data/EmpDAO.cs
@@ -10,7 +10,46 @@ namespace WebApplication1.Data
     {
         private string connectionString = @"Data Source=CHAMSTER\SQLEXPRESS;Initial Catalog=EmployeeDB;Integrated Security=True";
 
-     
+        // READ ALL
+        public List<EmployeeModel> GetAllEmployees()
+        {
+            List<EmployeeModel> list = new List<EmployeeModel>();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM mytable";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    list.Add(new EmployeeModel
+                    {
+                        EEID = reader["EEID"].ToString(),
+                        FullName = reader["Full Name"].ToString(),
+                        JobTitle = reader["Job Title"].ToString(),
+                        Department = reader["Department"].ToString(),
+                        BusinessUnit = reader["Business Unit"].ToString(),
+                        Gender = reader["Gender"].ToString(),
+                        Ethnicity = reader["Ethnicity"].ToString(),
+                        Age = Convert.ToInt32(reader["Age"]),
+                        HireDate = Convert.ToDateTime(reader["Hire Date"]),
+                        AnnualSalary = reader["Annual Salary"].ToString(),
+                        Bonus = reader["Bonus "].ToString(),
+                        Country = reader["Country"].ToString(),
+                        City = reader["City"].ToString(),
+                        ExitDate = reader["Exit Date"] == DBNull.Value ? null : (DateTime?)reader["Exit Date"]
+                    });
+                }
+
+                reader.Close();
+            }
+
+            return list;
+        }
+
         // READ ONE
         public EmployeeModel GetEmployeeById(string id)
         {
