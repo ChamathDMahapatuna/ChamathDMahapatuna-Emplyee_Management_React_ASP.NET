05b342a4e3fa6af02cfd44c934d56b0a9542e51c
M.D.C.D.Mahapatuna <109390299+ChamathDMahapatuna@users.noreply.github.com>
2025-06-13 17:59:39 +0530

    test

---
+++ employee-manager/EMP_SYS/src/Components/EmployeeDetails.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { getAllEmployees } from '../Api';
+
+function EmployeeList() {
+  const [employees, setEmployees] = useState([]);
+
+  useEffect(() => {
+    getAllEmployees().then((res) => {
+      setEmployees(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h2>Employees</h2>
+      <ul>
+        {employees.map(emp => (
+          <li key={emp.eeid}>
+            {emp.fullName} - {emp.jobTitle}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default EmployeeList;
---
+++ employee-manager/EMP_SYS/src/services/employeeService.js
@@ -0,0 +1,24 @@
+
+import axios from 'axios';
+
+const API_URL = 'https://localhost:44380/api/values';
+
+export const getEmployees = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    throw error; // Let the caller handle the error
+  }
+};
+
+export const getEmployeeById = async (id) => {
+  try {
+    const response = await axios.get(`${API_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error; // Let the caller handle the error
+  }
+}
+
+
